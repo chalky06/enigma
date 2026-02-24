a21b0de41dc1d1380bac37e44e9c7b77c6b15000
chalky06 <152178285+chalky06@users.noreply.github.com>
2026-02-24 18:22:24 +0000

    Update crypto.js

--- js/crypto.js
+++ js/crypto.js
@@ -1,13 +1,13 @@
 function checkCode() {
     const input = document.getElementById('userInput').value.toLowerCase();
     const feedback = document.getElementById('feedback');
-
+    
     const secretCode = "gargoyle1348";
 
     if(input === secretCode) {
-        // Redirect to Puzzle 1
+        feedback.textContent = "Correct"
         window.location.href = "puzzles/puzzle1.html";
     } else {
-        feedback.textContent = "❌ Incorrect. Try again!";
+        feedback.textContent = "Incorrect";
     }
 }
