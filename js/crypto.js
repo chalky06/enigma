f93e3d00e399b68872959b0e5ddf7c118dd4f3d6
chalky06 <152178285+chalky06@users.noreply.github.com>
2026-02-24 19:58:37 +0000

    Update crypto.js

--- js/crypto.js
+++ js/crypto.js
@@ -1,13 +1,18 @@
 function checkCode() {
     const input = document.getElementById('userInput').value.toLowerCase();
     const feedback = document.getElementById('feedback');
-    
-    const secretCode = "gargoyle1348";
 
-    if(input === secretCode) {
-        feedback.textContent = "Correct"
+    const secretCode = "gargoyles1348";
+
+    if (input === secretCode) {
+        feedback.textContent = "✅ Correct!";
+        feedback.style.color = "#188d45"; // dark green for correct
+        // redirect after a short delay
+        setTimeout(function() {
             window.location.href = "puzzles/puzzle1.html";
+        }, 500);
     } else {
-        feedback.textContent = "Incorrect";
+        feedback.textContent = "❌ Incorrect!";
+        feedback.style.color = "#de1c42"; // red for incorrect
     }
 }
